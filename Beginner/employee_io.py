61d95c0c97e4a9707c6d549f998372a85270a7e3
Manav7204 <manavpatil47@gmail.com>
2026-07-04 17:58:04 +0530

    Employee Information

---
+++ Beginner/employee_io.py
@@ -0,0 +1,13 @@
+emp_id = int(input("Enter Employee ID: "))
+emp_name = input("Enter name: ")
+age = int(input("Enter Employee age: "))
+emp_department = input("Enter Department: ")
+emp_salary = float(input("Enter Salary: "))
+
+print("\n\n_Employee Details_")
+print("\n------------------")
+print(f"Employee ID = {emp_id}")
+print(f"Employee Name = {emp_name}")
+print(f"Employee Age = {age}")
+print(f"Employee Department = {emp_department}")
+print(f"Employee Salary = {emp_salary}")
