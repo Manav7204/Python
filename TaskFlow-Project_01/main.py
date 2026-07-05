7e2533e908069dff20c9d4c38541d224b72db07f
Manav7204 <manavpatil47@gmail.com>
2026-07-05 20:51:44 +0530

    Implement Task Creation

--- TaskFlow-Project_01/main.py
+++ TaskFlow-Project_01/main.py
@@ -1,1 +1,27 @@
 print("Welcome to TaskFlow")
+
+task_list = []
+while True:
+    print("="*5, "TaskFlow", "="*5)
+    print("1. Add Task\n2.Exit")
+
+    try:
+        choice = int(input("Enter the number of operation: "))
+    except ValueError:
+        print("Invalid input. Enter an integer.")
+        continue
+
+    if choice == 1:
+        task_title = input("Enter the Title of the task.\n>").strip()
+        if task_title:
+            task_list.append(task_title)
+            print("Task added successfully.")
+        else:
+            print("Task cannot be empty!")
+    elif choice == 2:
+        print("Goodbye!")
+        break
+    else:
+        print("Enter Valid Input !")
+
+print(task_list)
