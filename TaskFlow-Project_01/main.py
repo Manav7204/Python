d3e5672458842fcaf7f6cba6dbf96bb666290ca7
Manav7204 <manavpatil47@gmail.com>
2026-07-05 21:14:49 +0530

    refactor menu into Functions

--- TaskFlow-Project_01/main.py
+++ TaskFlow-Project_01/main.py
@@ -1,27 +1,63 @@
 print("Welcome to TaskFlow")
 
-task_list = []
-while True:
+def show_menu():
     print("="*5, "TaskFlow", "="*5)
     print("1. Add Task\n2.Exit")
 
+def get_menu_choice():
     try:
         choice = int(input("Enter the number of operation: "))
     except ValueError:
-        print("Invalid input. Enter an integer.")
-        continue
+        print("\nInvalid input. Enter an integer.\n")
+        return None
+    
+    return choice
+
+def add_task(task_list):
+    task_title = input("Enter the Title of the task.\n>").strip()
+    if task_title:
+        task_list.append(task_title)
+        print("\nTask added successfully.\n")
+    else:
+        print("\nTask cannot be empty!\n")
+
+#-----------------------------------------------------------------------#
+
+task_list = []  
+
+while True:
+
+    show_menu()
+
+    choice = get_menu_choice()
 
     if choice == 1:
-        task_title = input("Enter the Title of the task.\n>").strip()
-        if task_title:
-            task_list.append(task_title)
-            print("Task added successfully.")
-        else:
-            print("Task cannot be empty!")
+        add_task(task_list)
     elif choice == 2:
         print("Goodbye!")
         break
     else:
-        print("Enter Valid Input !")
+        print("\nEnter Valid Input!\n")
 
-print(task_list)
+# while True:
+#     print("="*5, "TaskFlow", "="*5)
+#     print("1. Add Task\n2.Exit")
+
+#     try:
+#         choice = int(input("Enter the number of operation: "))
+#     except ValueError:
+#         print("Invalid input. Enter an integer.")
+#         continue
+
+#     if choice == 1:
+#         task_title = input("Enter the Title of the task.\n>").strip()
+#         if task_title:
+#             task_list.append(task_title)
+#             print("Task added successfully.")
+#         else:
+#             print("Task cannot be empty!")
+#     elif choice == 2:
+#         print("Goodbye!")
+#         break
+#     else:
+#         print("Enter Valid Input !")
