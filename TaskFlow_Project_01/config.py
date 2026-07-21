9096bc9c9284c598bd6e340ae65e6ae4476f7fcf
Manav7204 <manavpatil47@gmail.com>
2026-07-21 22:51:01 +0530

    Adding configuration and dotenv

--- TaskFlow_Project_01/config.py
+++ TaskFlow_Project_01/config.py
@@ -1,12 +1,14 @@
 import logging
+from dotenv import load_dotenv
+import os
 
-LOG_FILE = "logs/taskflow.log"
-LOG_LEVEL = logging.DEBUG
-LOG_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
+load_dotenv()
+
+DATABASE_NAME = os.getenv("DATABASE_NAME")
+LOG_FILE = os.getenv("LOG_FILE")
+LOG_LEVEL = getattr(logging, os.getenv("LOG_LEVEL"))
+LOG_FORMAT = os.getenv("LOG_FORMAT")
 
 
 def setup_logging():
     logging.basicConfig(filename=LOG_FILE, level=LOG_LEVEL, format=LOG_FORMAT)
-
-
-DATABASE_NAME = "data/task.db"
